8edb340a8ce0befbc78b677168798e01cbd28e10
kishan-964 <computervictus5@gmail.com>
2026-07-14 21:55:06 +0545

    update app.js and .env

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     
